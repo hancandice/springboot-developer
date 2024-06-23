1ae6536e55143d68ac90a9c5401587b0f8c53a8d
hancandice <jeeyoung.han82@gmail.com>
2024-06-23 09:03:22 +0200

    Add BlogApiControllerTest test code

---
+++ src/test/java/me/jeehahn/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -0,0 +1,17 @@
+package me.jeehahn.springbootdeveloper.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class BlogApiControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
