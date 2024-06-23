05ec44c19b15e5ed91e489e30750179ce914c8ff
hancandice <jeeyoung.han82@gmail.com>
2024-06-23 21:35:31 +0200

    add findAllArticles() test

--- src/test/java/me/jeehahn/springbootdeveloper/controller/BlogApiControllerTest.java
+++ src/test/java/me/jeehahn/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -1,6 +1,8 @@
 package me.jeehahn.springbootdeveloper.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -75,4 +77,44 @@ class BlogApiControllerTest {
         assertThat(articles.get(0).getTitle()).isEqualTo(title);
         assertThat(articles.get(0).getContent()).isEqualTo(content);
     }
+
+
+    @DisplayName("findAllArticles: Successfully retrieves all articles")
+    @Test
+    public void findAllArticles() throws Exception {
+
+        // Given
+        final String url = "/api/articles";
+        final String title1 = "title 1";
+        final String content1 = "content 1";
+        final String title2 = "title 2";
+        final String content2 = "content 2";
+
+        Article article1 = Article.builder().title(title1).content(content1).build();
+        Article article2 = Article.builder().title(title2).content(content2).build();
+        blogRepository.save(article1);
+        blogRepository.save(article2);
+
+        // When
+        ResultActions result = mockMvc.perform(get(url)
+            .accept(MediaType.APPLICATION_JSON));
+
+        // Then
+        result.andExpect(status().isOk())
+            .andExpect(jsonPath("$.length()").value(2))
+            .andExpect(jsonPath("$[0].title").value(title1))
+            .andExpect(jsonPath("$[0].content").value(content1))
+            .andExpect(jsonPath("$[1].title").value(title2))
+            .andExpect(jsonPath("$[1].content").value(content2));
+
+        List<Article> articles = blogRepository.findAll();
+        assertThat(articles).hasSize(2);
+        assertThat(articles.get(0).getTitle()).isEqualTo(title1);
+        assertThat(articles.get(0).getContent()).isEqualTo(content1);
+        assertThat(articles.get(1).getTitle()).isEqualTo(title2);
+        assertThat(articles.get(1).getContent()).isEqualTo(content2);
+
+    }
+
+
 }
