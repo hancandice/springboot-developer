6c42bd28509bc4e8a87708c397b721b5789454ec
hancandice <jeeyoung.han82@gmail.com>
2024-06-30 20:54:41 +0200

    fix: signup() method should handle form submissions using application/x-www-form-urlencoded

--- src/main/java/me/jeehahn/springbootdeveloper/controller/UserApiController.java
+++ src/main/java/me/jeehahn/springbootdeveloper/controller/UserApiController.java
@@ -10,7 +10,6 @@ import org.springframework.security.web.authentication.logout.SecurityContextLog
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
@@ -20,8 +19,16 @@ public class UserApiController {
     private final UserService userService;
 
     @PostMapping("/user")
-    public String signup(@RequestBody AddUserRequest request) {
-        userService.save(request);
+    public String signup(HttpServletRequest request) {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        AddUserRequest addUserRequest = new AddUserRequest();
+        addUserRequest.setEmail(email);
+        addUserRequest.setPassword(password);
+
+        userService.save(addUserRequest);
+
         return "redirect:/login";
     }
 
