8cfb75fcef164321908801380d59854b4e655f8f
hancandice <jeeyoung.han82@gmail.com>
2024-07-22 14:53:58 +0200

    Make test code for TokenApiController

---
+++ src/test/java/me/jeehahn/springbootdeveloper/controller/TokenApiControllerTest.java
@@ -0,0 +1,12 @@
+package me.jeehahn.springbootdeveloper.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TokenApiControllerTest {
+
+    @Test
+    void createNewAccessToken() {
+    }
+}
